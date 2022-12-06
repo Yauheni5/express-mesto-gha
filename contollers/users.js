4f7e5c3f4fb4397e1bd2d80fc41aff283907a81c
Yauheni5 <yauheni5@github.com>
2022-12-06 20:38:41 +0300

    fix: check error

--- contollers/users.js
+++ contollers/users.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(() => res.status(200).send({ data: { name, about, avatar } }))
+    .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные` });
