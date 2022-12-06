de72b4bc7b984e0f4b84e2ced7b4f3c93be043df
Yauheni5 <yauheni5@github.com>
2022-12-06 20:28:56 +0300

    fix: bugs after autotest

--- contollers/users.js
+++ contollers/users.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(200).send({ data: user, message: 'Пользователь создан' }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные` });
@@ -18,12 +18,13 @@ module.exports.createUser = (req, res) => {
 
 module.exports.findUser = (req, res) => {
   User.findById(req.params._id)
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(200).send({ data: user, message: 'Пользователь найден' }))
     .catch((err) => {
+      console.log(err.name);
       if (err.name === 'Error') {
-        res.status(400).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные при обновлении профиля.` });
+        res.status(404).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные при обновлении профиля.` });
       } else if (err.name === 'CastError') {
-        res.status(404).send({ message: `Произошла ошибка: ${err.name}. Пользователь с указанным _id не найден.` });
+        res.status(400).send({ message: `Произошла ошибка: ${err.name}. Пользователь с указанным _id не найден.` });
       } else {
         res.status(500).send({ message: `Произошла ошибка: ${err.name}` });
       }
@@ -32,7 +33,7 @@ module.exports.findUser = (req, res) => {
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((user) => res.status(200).send({ data: user }))
+    .then((user) => res.status(200).send({ data: user, message: 'Зарегистрированные пользователи' }))
     .catch((err) => {
       res.status(500).send({ message: `Произошла ошибка: ${err.name}` });
     });
@@ -46,12 +47,12 @@ module.exports.updateProfile = (req, res) => {
     { name, about },
     { new: true, runValidators: true, setDefaultsOnInsert: true },
   )
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({ data: user, message: 'Профиль обновлён' }))
     .catch((err) => {
       if (err.name === 'Error') {
-        res.status(400).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные при обновлении профиля.` });
+        res.status(404).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные при обновлении профиля.` });
       } else if (err.name === 'CastError') {
-        res.status(404).send({ message: `Произошла ошибка: ${err.name}. Пользователь с указанным _id не найден.` });
+        res.status(400).send({ message: `Произошла ошибка: ${err.name}. Пользователь с указанным _id не найден.` });
       } else {
         res.status(500).send({ message: `Произошла ошибка: ${err.name}` });
       }
@@ -62,12 +63,12 @@ module.exports.updateAvatar = (req, res) => {
   const userID = req.user._id;
   const { avatar } = req.body;
   User.findByIdAndUpdate({ userID, $set: { avatar } })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send({ data: user, message: 'Аватар пользователя обновлён' }))
     .catch((err) => {
       if (err.name === 'Error') {
-        res.status(400).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные при обновлении аватара.` });
+        res.status(404).send({ message: `Произошла ошибка: ${err.name}. Переданы некорректные данные при обновлении аватара.` });
       } else if (err.name === 'CastError') {
-        res.status(404).send({ message: `Произошла ошибка: ${err.name}. Пользователь с указанным _id не найден.` });
+        res.status(400).send({ message: `Произошла ошибка: ${err.name}. Пользователь с указанным _id не найден.` });
       } else {
         res.status(500).send({ message: `Произошла ошибка: ${err.name}` });
       }
