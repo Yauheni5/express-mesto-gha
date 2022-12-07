7d273cdd9f458e6f47e642d0f5a8a554a0df7564
Yauheni5 <yauheni5@github.com>
2022-12-07 17:07:25 +0300

    fix: after review

--- contollers/cards.js
+++ contollers/cards.js
@@ -1,5 +1,14 @@
 const Card = require('../models/Card');
-const errorHandler = require('../errors/errors');
+
+const errorHandler = (err, res) => {
+  if (err.name === 'DocumentNotFoundError') {
+    res.status(404).send({ message: 'Данные по переданному Id не найдены' });
+  } else if (err.name === 'CastError' || 'ValidationError') {
+    res.status(400).send({ message: 'Переданы некорректные данные в метод' });
+  } else {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
 
 module.exports.createCard = (req, res) => {
   const ownerId = req.user._id;
--- contollers/users.js
+++ contollers/users.js
@@ -2,7 +2,16 @@
 // файл контроллеров
 
 const User = require('../models/User');
-const errorHandler = require('../errors/errors');
+
+const errorHandler = (err, res) => {
+  if (err.name === 'DocumentNotFoundError') {
+    res.status(404).send({ message: 'Данные по переданному Id не найдены' });
+  } else if (err.name === 'CastError' || 'ValidationError') {
+    res.status(400).send({ message: 'Переданы некорректные данные в метод' });
+  } else {
+    res.status(500).send({ message: 'Произошла ошибка' });
+  }
+};
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
