2cac802cbb37178ec6ee3506bcd374ef08840518
Yauheni5 <yauheni5@github.com>
2022-12-17 20:54:00 +0300

    fix

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
-    avatar: Joi.string().dataUri().default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+    avatar: Joi.default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png').dataUri(),
   }),
 }), createUser);
 
