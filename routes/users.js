e0319b389aa5837e3e6bfb6790adde0d01445cc9
Yauheni5 <yauheni5@github.com>
2022-12-18 12:49:56 +0300

    fix: minlength data

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,7 @@ usersRoutes.get('/:_id', celebrate({
 }), findUser);
 
 usersRoutes.patch('/me', celebrate({
-  params: Joi.object().keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
